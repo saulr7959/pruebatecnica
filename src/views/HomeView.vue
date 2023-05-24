70c34ec2c36015e095c56ab80d20167842164adb
Saul Osorio <70832648+saulr7959@users.noreply.github.com>
2023-05-24 12:10:07 -0600

    Update HomeView.vue
    Cambie código por modal

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,40 @@
+
 <template>
   <a-row type="flex">
-      <br>
-      
-      <a-col flex="auto" >
-          <div class="search-wrapper">
-              <input type="text" v-model="nombrePais" placeholder="Escriba el nombre de un país" />
-              <button @click="buscar">Buscar</button>
-          </div>
-          <a-table :dataSource="paises" :columns="columnas">
-              <template #bandera="{ text }">
-                  <img :src="text" alt="Bandera" class="flag-img" style="width: 20px; height: auto;" />
-              </template>
-              <template #accion="{ text }">
-                  <div>
-                      <a-button type="primary" @click="mostrarModal(text)">Detalles</a-button>
-                      <a-modal v-model:visible="visible" title="Detalles del país" @ok="aceptarModal">
-                          <div v-if="paisSeleccionado">
-                              <h4>Bandera:</h4>
-                              <img :src="paisSeleccionado.bandera" class="flag-img" style="width: 100px; height: auto;">
-                              <h1>Información:</h1>
-                              <h2>{{ paisSeleccionado.nombre }}</h2>
-                              <h4>Capital:</h4>
-                              <span>{{ paisSeleccionado.capital }}</span>
+    <br />
 
-                              <h4>Población:</h4>
-                              <span>{{ paisSeleccionado.poblacion }}</span>
-                          </div>
-                      </a-modal>
-                  </div>
-              </template>
-          </a-table>
-      </a-col>
-      <a-col flex="100px" >
-      
-          <div id="result" v-html="detallesPais"></div>
-      </a-col>
+    <a-col flex="auto">
+      <div class="search-wrapper">
+        <input type="text" v-model="nombrePais" placeholder="Escriba el nombre de un país" />
+        <button @click="buscar">Buscar</button>
+      </div>
+      <a-table :dataSource="paises" :columns="columnas">
+        <template #bandera="{ text }">
+          <img :src="text" alt="Bandera" class="flag-img" style="width: 20px; height: auto;" />
+        </template>
+        <template #accion="{ text }">
+          <div>
+            <a-button type="primary" @click="mostrarModal(text)">Detalles</a-button>
+            <a-modal v-model:visible="visible" title="Detalles del país" @ok="aceptarModal">
+              <div v-if="paisSeleccionado">
+                <h4>Bandera:</h4>
+                <img :src="paisSeleccionado.bandera" class="flag-img" style="width: 100px; height: auto;">
+                <h1>Información:</h1>
+                <h2>{{ paisSeleccionado.nombre }}</h2>
+                <h4>Capital:</h4>
+                <span>{{ paisSeleccionado.capital }}</span>
+
+                <h4>Población:</h4>
+                <span>{{ paisSeleccionado.poblacion }}</span>
+              </div>
+            </a-modal>
+          </div>
+        </template>
+      </a-table>
+    </a-col>
+    <a-col flex="100px">
+      <div id="result" v-html="detallesPais"></div>
+    </a-col>
   </a-row>
 </template>
 
@@ -45,93 +45,93 @@ import { defineComponent, ref } from 'vue';
 
 export default {
   setup() {
-      const visible = ref(false);
-      const paisSeleccionado = ref(null);
+    const visible = ref(false);
+    const paisSeleccionado = ref(null);
 
-      const mostrarModal = (pais) => {
-          paisSeleccionado.value = pais;
-          console.log(paisSeleccionado)
-          visible.value = true;
-      };
+    const mostrarModal = (pais) => {
+      paisSeleccionado.value = pais;
+      console.log(paisSeleccionado);
+      visible.value = true; // Agregar esta línea para mostrar el modal
+    };
 
-      const aceptarModal = e => {
-          console.log(e);
-          visible.value = false;
-      };
+    const aceptarModal = (e) => {
+      console.log(e);
+      visible.value = false;
+    };
 
-      return {
-          visible,
-          paisSeleccionado,
-          mostrarModal,
-          aceptarModal,
-      };
+    return {
+      visible,
+      paisSeleccionado,
+      mostrarModal,
+      aceptarModal,
+    };
   },
 
   data() {
-      return {
-          paises: null,
-          columnas: [
-              {
-                  title: 'Banderas',
-                  dataIndex: 'bandera',
-                  key: 'bandera',
-                  slots: { customRender: 'bandera' },
-              },
-              {
-                  title: 'País',
-                  dataIndex: 'nombre',
-                  key: 'nombre',
-              },
-              {
-                  title: 'Población',
-                  dataIndex: 'poblacion',
-                  key: 'poblacion',
-              },
-              {
-                  title: 'Acción',
-                  dataIndex: 'accion',
-                  key: 'accion',
-                  slots: { customRender: 'accion' },
-              },
-          ],
-          nombrePais: '',
-          detallesPais: '',
-      };
+    return {
+      paises: null,
+      columnas: [
+        {
+          title: 'Banderas',
+          dataIndex: 'bandera',
+          key: 'bandera',
+          slots: { customRender: 'bandera' },
+        },
+        {
+          title: 'País',
+          dataIndex: 'nombre',
+          key: 'nombre',
+        },
+        {
+          title: 'Población',
+          dataIndex: 'poblacion',
+          key: 'poblacion',
+        },
+        {
+          title: 'Acción',
+          dataIndex: 'accion',
+          key: 'accion',
+          slots: { customRender: 'accion' },
+        },
+      ],
+      nombrePais: '',
+      detallesPais: '',
+    };
   },
 
   mounted() {
-      this.obtenerPaises();
+    this.obtenerPaises();
   },
 
   methods: {
-      obtenerPaises() {
-          axios
-              .get('https://restcountries.com/v3.1/all')
-              .then((response) => {
-                  this.paises = response.data.map((pais, index) => ({
-                      key: index.toString(),
-                      bandera: pais.flags.svg,
-                      nombre: pais.name.common,
-                      poblacion: pais.population,
-                  }));
-              })
-              .catch((error) => {
-                  console.error('Error al obtener los países:', error);
-              });
-      },
+    obtenerPaises() {
+      axios
+        .get('https://restcountries.com/v3.1/all')
+        .then((response) => {
+          this.paises = response.data.map((pais, index) => ({
+            key: index.toString(),
+            bandera: pais.flags.svg,
+            nombre: pais.name.common,
+            poblacion: pais.population,
+          }));
+        })
+        .catch((error) => {
+          console.error('Error al obtener los países:', error);
+        });
+    },
 
-      buscar() {
-          if (this.nombrePais.length === 0) {
-              this.detallesPais = '<h3>POR FAVOR, COMPLETE LA INFORMACIÓN</h3>';
-              return;
-          }
+    buscar() {
+      if (this.nombrePais.length === 0) {
+        this.detallesPais = '<h3>POR FAVOR, COMPLETE LA INFORMACIÓN</h3>';
+        return;
+      }
 
-          const finalURL = `https://restcountries.com/v3.1/name/${this.nombrePais}?fullText=true`;
-          axios
-              .get(finalURL)
-              .then((response) => {
-                  const data = response.data[0];
-                  this.detallesPais = `
+      const finalURL = `https://restcountries.com/v3.1/name/${this.nombrePais}?fullText=true`;
+      axios
+        .get(finalURL)
+        .then((response) => {
+          const data = response.data[0];
+          this.detallesPais = `
           <h4>Bandera:</h4>
             <img src="${data.flags.svg}" class="flag-img" style="width: 100px; height: auto;">
             <h1>Información:</h1>
@@ -155,11 +155,11 @@ export default {
               </div>
             </div>
           `;
-              })
-              .catch(() => {
-                  this.detallesPais = '<h3>POR FAVOR INGRESE UN PAÍS VÁLIDO.</h3>';
-              });
-      },
+        })
+        .catch(() => {
+          this.detallesPais = '<h3>POR FAVOR INGRESE UN PAÍS VÁLIDO.</h3>';
+        });
+    },
   },
 };
 </script>
@@ -167,4 +167,3 @@ export default {
 <style>
 /* Estilos adicionales */
 </style>
-
