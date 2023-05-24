13d3e681b87633a2a4277587afebeed62502e43e
Saul Osorio <saulr7959@gmail.com>
2023-05-24 06:42:59 -0600

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,9 @@
 <template>
-  
+  <div style="background: #ececec; padding: 30px">
+    <a-card title="CREADOR" :bordered="false" style="width: 300px">
+      <p>SAUL ERNESTO OSORIO RODRIGUEZ</p>
+      <p>INGENIERO EN CIENCIAS DE LA COMPUTACION</p>
+      <p>Card content</p>
+    </a-card>
+  </div>
 </template>
